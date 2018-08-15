333513289b5fe10b0d5d79d8307e9dcf4b06c7f3
JaneJuma <jaynejumah@gmail.com>
2018-08-15 15:05:06 +0300

    changes and validation

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 from flask import Flask, render_template, url_for
+from flask import request
+user_details = {}
+
 
 app= Flask (__name__)
+app.config['SECRET_KEY'] = 'DontTellAnyone'
 
 @app.route('/')
 def landing():
     return render_template ('landing.html')
 
 
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 def index():
     return render_template ('index.html')
 
 
-@app.route('/registration')
+@app.route('/login_validation', methods=['GET','POST'])
+def login_validation():
+    email = request.values['email']
+    password = request.values['password']
+    if email in user_details.keys():
+        if password == user_details[email][2]:
+            return render_template('display.html')
+        else:
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
+   
+
+@app.route('/registration', methods=['GET','POST'])
 def registration():
     return render_template ('registration.html')
+
+
+@app.route('/registration_validation', methods=['GET','POST'])
+def registration_validation():
+    firstname = request.values['firstname']
+    lastname = request.values['lastname']
+    email = request.values['email']
+    password = request.values['password']
+
+    user_details[email] = (firstname, lastname, email, password)
+    return render_template('index.html')
+
+
+
 
 
 @app.route('/display')
